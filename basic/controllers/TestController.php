0aa006eb2ac878d9cd5dcce4a5d2676a1495f4ba
AaronSaat <aaron.fian@seabs.ac.id>
2025-02-25 09:46:44 +0700

    coba conflict 4
    oke bisa gitu

--- basic/controllers/TestController.php
+++ basic/controllers/TestController.php
@@ -2,14 +2,14 @@ test ini dari master
 
 coba dari laptop   
 
-<<<<<<< Updated upstream
+
 coba dari pc 6 kirim ke laptop hahahah 092139
 
 testing tabrakan kajh
-=======
+
 coba dari pc 6 kirim ke laptop hahahah  testing yey
 
 halohlao
 
 heheheh
->>>>>>> Stashed changes
+
